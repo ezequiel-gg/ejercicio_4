d95bdad135f6bf6ecdbf41d01be13d6142b2b6f6
ezequiel-gg <54220485+ezequiel-gg@users.noreply.github.com>
2020-09-08 19:45:42 -0300

    wip

---
+++ Library/GameBoard.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ejercicio_4
+{
+    public class GameBoard 
+    {
+        public GameBoard(double precioMasa, double precioDulce)
+        {
+
+        }
+
+    }
+}
---
+++ Library/PrintBoard.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace ejercicio_4
+{
+    public class PrintBoard
+    {
+        public Boolean Print()
+        {
+            bool[,] b; //variable que representa el tablero
+            int width; //variabe que representa el ancho del tablero
+            int height; //variabe que representa altura del tablero
+            While(true);
+            {
+                Console.Clear();
+                StringBuilder s = new StringBuilder();
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        if (b[x, y])
+                        {
+                            s.Append("|X|");
+                        }
+                        else
+                        {
+                            s.Append("___");
+                        }
+                    }
+                    s.Append("\n");
+                }
+                Console.WriteLine(s.ToString());
+                //=================================================
+                //Invocar método para calcular siguiente generación
+                //=================================================
+                Thread.Sleep(300);
+            }
+        }
+
+    }
+}
